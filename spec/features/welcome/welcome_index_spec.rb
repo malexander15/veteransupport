311fc36dcc2d7bafda11800d3905ee30a61f4c82
Zane Sandin <i.zane.am.robot@gmail.com>
2018-02-10 13:06:28 -0500

    Adds test scenario to check for navbar elements

--- spec/features/welcome/welcome_index_spec.rb
+++ spec/features/welcome/welcome_index_spec.rb
@@ -1,10 +1,22 @@
 require "rails_helper"
 
 RSpec.feature "Welcome Index Page", :type => :feature do
-  scenario "Unauthenticated user visits welcome page" do
+  let(:navbar_links) do
+    %w[ V.E.T. Home Veteran\ Support V.E.T.\ Radio About Shop\ V.E.T.
+    ]
+  end
+
+  it "Unauthenticated user visits welcome page" do
     visit welcome_index_path
     expect(page).to have_text(
       "Welcome#index Find me in app/views/welcome/index.html.erb"
     )
+  end
+
+  it "has expected navbar elements" do
+    visit welcome_index_path
+    navbar_links.each do |link_text|
+      expect(page).to have_link(link_text)
+    end
   end
 end
