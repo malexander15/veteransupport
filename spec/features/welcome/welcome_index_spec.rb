dda304de315bf01da762da9ebd834a2d0cfb0aab
Zane Sandin <i.zane.am.robot@gmail.com>
2018-02-10 13:03:37 -0500

    removed conflicting css and fixed the welcome index spec test

--- spec/features/welcome/welcome_index_spec.rb
+++ spec/features/welcome/welcome_index_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.feature "Welcome Index Page", :type => :feature do
   scenario "Unauthenticated user visits welcome page" do
     visit welcome_index_path
-    expect(page).to have_text("Sample Text for the Welcome Page")
+    expect(page).to have_text(
+      "Welcome#index Find me in app/views/welcome/index.html.erb"
+    )
   end
 end
